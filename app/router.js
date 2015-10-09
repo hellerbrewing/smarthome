ff3619259cea53a69d79c055e9a65a81d5b01f7c
hellerbrewing <steve_heller82@hotmail.com>
2015-10-08 19:51:57 -0700

    navigation stubbed out

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('lighting');
+  this.route('thermostat');
+  this.route('garageDoor');
+  this.route('cameras');
+  this.route('light-switch');
+  this.route('light-scene');
 });
 
 export default Router;
