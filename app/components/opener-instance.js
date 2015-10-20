fde7239c0ec0972936ba5eb9d5a13a05d84bde59
hellerbrewing <steve_heller82@hotmail.com>
2015-10-19 19:18:47 -0700

    thermosstat partially set up

--- app/components/opener-instance.js
+++ app/components/opener-instance.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	didtoggle: function (){
+	didToggle: function (){
 		console.log("garageopener: "+this.get("garageopener").get("name")+" value="+this.get("garageopener").get("on"))
 		this.get ("garageopener").save()
 	}.observes("garageopener.open")
